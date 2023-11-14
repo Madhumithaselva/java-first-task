f2b5d78cd601a13c678419a2b7c483fa03b513c8
Madhu <madhu8129@gmail.com>
2023-11-14 22:13:32 +0100

    Addtion of 2 decimals code is added in the file

--- src/main/java/se/lexicon/Main.java
+++ src/main/java/se/lexicon/Main.java
@@ -7,9 +7,14 @@ public class Main {
         System.out.println("Hello "+ firstName);*/
 
 
-        //Creating 2 strings and separating it with space delimiter
+        /*Creating 2 strings and separating it with space delimiter
         String firstName="Madhumitha";
         String lastName="Selvaraj";
-        System.out.println("Hello "+ firstName +" "+lastName);
+        System.out.println("Hello "+ firstName +" "+lastName);*/
+
+        //Addition of two variables
+        int a=15,b=25,c;
+        c=a+b;
+        System.out.println("Addition of given 2 no.s are "+c);
     }
 }
