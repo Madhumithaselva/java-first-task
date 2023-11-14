b6f6ae54dbb26a9f93eb5708747cac1ac1b4cace
Madhu <madhu8129@gmail.com>
2023-11-14 22:09:16 +0100

    Added two strings task in the program file

--- src/main/java/se/lexicon/Main.java
+++ src/main/java/se/lexicon/Main.java
@@ -2,12 +2,14 @@ package se.lexicon;
 
 public class Main {
     public static void main(String[] args) {
-        /*Creating a new String and printing it*/
+        /*Creating a new String and printing it
         String firstName="Madhumitha";
-        System.out.println("Hello "+ firstName);
+        System.out.println("Hello "+ firstName);*/
 
 
         //Creating 2 strings and separating it with space delimiter
-
+        String firstName="Madhumitha";
+        String lastName="Selvaraj";
+        System.out.println("Hello "+ firstName +" "+lastName);
     }
 }
