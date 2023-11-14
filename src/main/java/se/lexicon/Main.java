7b7cdd55c857c9fde9d826182fec79630ddbf4da
Madhu <madhu8129@gmail.com>
2023-11-14 22:04:28 +0100

    Created a hello string program file

---
+++ src/main/java/se/lexicon/Main.java
@@ -0,0 +1,13 @@
+package se.lexicon;
+
+public class Main {
+    public static void main(String[] args) {
+        /*Creating a new String and printing it*/
+        String firstName="Madhumitha";
+        System.out.println("Hello "+ firstName);
+
+
+        //Creating 2 strings and separating it with space delimiter
+
+    }
+}
