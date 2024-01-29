b257787ba8e933ba7463946bc319ef402da106ef
Madhu <madhu8129@gmail.com>
2024-01-29 10:23:24 +0100

    new branch commit

--- src/main/java/se/lexicon/Main.java
+++ src/main/java/se/lexicon/Main.java
@@ -12,9 +12,29 @@ public class Main {
         String lastName="Selvaraj";
         System.out.println("Hello "+ firstName +" "+lastName);*/
 
-        //Addition of two variables
+        /*Addition of two variables
         int a=15,b=25,c;
         c=a+b;
-        System.out.println("Addition of given 2 no.s are "+c);
+        System.out.println("Addition of given 2 no.s are "+c);*/
+
+        //Object
+        Car
+        System.out.println(getCarInformation());
     }
+        //Creating a class called car and defining its attributes
+        class Car{
+            String carBrand="Tesla";
+            String regNumber="ABC123";
+            String maxSpeed="200kmph";
+            String ownerName="Aditya";
+            int price=200000;
+            int yearModel=2020;
+
+            public String getCarInformation(){
+                return carBrand + "\n " +  regNumber + "\n" +maxspeed + "\n" + ownerName + "\n"+price+"\n"+yearModel;
+                            }
+        }
+
+
+
 }
